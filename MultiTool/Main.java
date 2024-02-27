06deb8c13c73b8d2ecddc02105474abc4984c052
AlxndrKatz <137706323+AlxndrKatz@users.noreply.github.com>
2024-02-27 23:35:02 +0700

    Update Main.java

--- MultiTool/Main.java
+++ MultiTool/Main.java
@@ -1,13 +1,11 @@
-package Research.MultiTool;
-
 public class Main {
     public static void main(String[] args) {
-        Tool tool = new Hammer(new Drill());
+        Tool tool = new Hammer(new Drill(null));
         tool.work();
 
         System.out.println("\n=====\n");
 
-        Tool tool1 = new Drill(new Hammer());
+        Tool tool1 = new Drill(new Hammer(null));
         tool1.work();
     }
 }
