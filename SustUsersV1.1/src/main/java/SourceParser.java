dd1cd92bec5c0567d199cf6c172e259027bc09f0
AlxndrKatz <137706323+AlxndrKatz@users.noreply.github.com>
2024-02-28 22:00:13 +0700

    Delete SustUsersV1.1/src/main/java/SourceParser.java

--- SustUsersV1.1/src/main/java/SourceParser.java
+++
@@ -1,8 +0,0 @@
-public class SourceParser {
-    public Consumer createConsumerData (String[] input) {
-        Consumer consumer = new Consumer(Integer.parseInt(input[0]), input[1], Integer.parseInt(input[2]),
-                Integer.parseInt(input[3]), Integer.parseInt(input[4]),
-                Integer.parseInt(input[5]), Integer.parseInt(input[6]));
-        return consumer;
-    }
-}
